90406fdfb1ed63d02c4b0850cab8491afd89fe7c
abc-valera <valeriy.tymofieiev@gmail.com>
2025-09-20 14:25:55 +0300

    Refactored

--- flake.nix
+++ flake.nix
@@ -1,10 +1,7 @@
 {
   description = "abc-valera's NixOS configuration";
 
-  # TODO: take a look on these:
-  # https://nixos-and-flakes.thiscute.world/nixos-with-flakes/nixos-flake-configuration-explained
-  # https://nixos-and-flakes.thiscute.world/nixos-with-flakes/nixos-flake-and-module-system
-  # https://nixos-and-flakes.thiscute.world/nixos-with-flakes/start-using-home-manager
+  # TODO: maybe move everything except hardware to home-manager??
 
   # TODO: configure using both stable and unstable channels
   # http://librephoenix.com/2024-02-10-using-both-stable-and-unstable-packages-on-nixos-at-the-same-time
@@ -29,17 +26,15 @@
       nixosConfigurations.abc-valera-elitebook25 = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
         modules = [
-          ./hardware-elitebook25.nix
-          ./configuration.nix
+          ./hardware/elitebook25.nix
+          ./configuration/configuration.nix
 
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
 
-            home-manager.users.abc-valera = import ./home.nix;
-
-            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+            home-manager.users.abc-valera = import ./home/abc-valera.nix;
           }
         ];
       };
