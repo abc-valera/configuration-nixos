df29005d29f7742b9166a61119454bea474ce35a
abc-valera <valeriy.tymofieiev@gmail.com>
2025-10-01 01:20:51 +0300

    Golang added

--- configuration/programming.nix
+++ configuration/programming.nix
@@ -6,7 +6,11 @@
   ...
 }:
 
+# TODO: Find a way to install things the way it's supposed to be, like rustup for Rust,
+# just installing golang, etc.
+
 {
+  # On programming languages setup: https://github.com/the-nix-way/dev-templates
   environment.systemPackages = [
     # Networking tools
     pkgs.wget
@@ -26,18 +30,39 @@
 
     # Basic dev tools
     pkgs.git
+    pkgs.gh
     pkgs.vim
     unstable.vscode
+    # pkgs.direnv
 
-    # Programming languages
-    unstable.dotnetCorePackages.sdk_9_0-bin
-
-    # Nix tools
+    # Nix
     pkgs.nixfmt-rfc-style
     pkgs.nixd
 
+    # Go
+    unstable.go
+    unstable.gopls
+    unstable.delve
+    unstable.gomodifytags
+    unstable.impl
+    unstable.gotests
+    unstable.go-tools
+    unstable.gotools
+
+    # Dotnet
+    unstable.dotnetCorePackages.sdk_9_0-bin
+
+    # Node.js
+    # unstable.nodejs
+
+    # Rust (https://wiki.nixos.org/wiki/Rust)
+    # unstable.cargo
+    # unstable.rustc
+
+    # Python
+
     # Shell-related
-    unstable.ghostty
+    pkgs.ghostty
     pkgs.fish
     pkgs.starship
   ];
